26bb3d88c007953e9a56b4947fb2c40d5c12399c
kny <kny030303>
2020-12-07 19:49:29 +0900

    complete

--- app/src/main/java/kr/hs/emirim/s2019s05/mirimgridviewtest/GridAdapter.java
+++ app/src/main/java/kr/hs/emirim/s2019s05/mirimgridviewtest/GridAdapter.java
@@ -14,6 +14,10 @@ public class GridAdapter extends BaseAdapter {
     int[] imgRes = {R.drawable.poster1,R.drawable.poster2,R.drawable.poster3,R.drawable.poster4,R.drawable.poster5,R.drawable.poster6,R.drawable.poster7
     ,R.drawable.poster8,R.drawable.poster9,R.drawable.poster10,R.drawable.poster11,R.drawable.poster12,R.drawable.poster13,R.drawable.poster14,R.drawable.poster15,
             R.drawable.poster16};
+
+    String[] imgName = {"삼진그룹영어토익반","소리도 없이","담보","아이랜드","코랠라인","기생충","시동"
+            ,"인터스텔라","검은 사제들","사자","늑대소년","너의이름은","컨져링","애나벨","해리포터 마법의돌",
+            "해리포터 비밀의방"};
     public GridAdapter(Context context){
         this.context = context;
     }
@@ -49,7 +53,7 @@ public class GridAdapter extends BaseAdapter {
                 AlertDialog.Builder dlg = new AlertDialog.Builder(context);
                 ImageView imgvPoster = dialogView.findViewById(R.id.img_dialog);
                 imgvPoster.setImageResource(imgRes[pos]);
-                dlg.setTitle("큰포스터");
+                dlg.setTitle(imgName[pos]);
                 dlg.setIcon(R.mipmap.ic_launcher);
                 dlg.setView(dialogView);
                 dlg.setNegativeButton("닫기",null);
